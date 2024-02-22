4ed2c1ce1440c3bdb23fd3e5493903e0863d72d3
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-02-22 21:46:53 +0100

    Menu na página inicial adicionada

--- resources/views/usuario/pagina_inicial.blade.php
+++ resources/views/usuario/pagina_inicial.blade.php
@@ -1,1 +1,21 @@
-<h1>Sessão inciada</h1>
+<title>Página Inicial</title>
+@include('usuario.inclusao.cabecalho')
+@include('usuario.inclusao.navbar_usuario_logado')
+
+<main>
+    <div class="container d-flex justify-content-center align-items-center" style="min-height: inherit">
+        <div class="">
+            <div class="col d-flex justify-content-center">
+                <i class="fas fa-shield-alt" style="font-size: 200px; color: green"></i>
+            </div>
+            <div class="col d-flex justify-content-center">
+                <p><b>Sessão inciada</b></p>
+            </div>
+        </div>
+    </div>
+</main>
+
+@include('usuario.inclusao.rodape')
+
+
+<h1></h1>
