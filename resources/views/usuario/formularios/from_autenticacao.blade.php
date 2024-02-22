1b1bb730296edf73d7df6a465405217423296e4b
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-02-22 19:50:59 +0100

    bootstrap adicionado no campo de autenticação

--- resources/views/usuario/formularios/from_autenticacao.blade.php
+++ resources/views/usuario/formularios/from_autenticacao.blade.php
@@ -1,23 +1,33 @@
 <div class="container " style="min-height: inherit; ">
-    <form class=" d-flex justify-content-center align-items-center" style="min-height: inherit;" action="/usuario/autenticacao" method="post">
+    <form class="needs-validation d-flex justify-content-center align-items-center" style="min-height: inherit;"
+        action="/usuario/autenticacao" method="post">
         @csrf
         <div class="col-6 ">
-            <h1>Autenticação</h1> <hr>
-                <div class="col mt-2">
-                    <label for="" style="color: dodgerblue"><b>Email:</b></label>
-                    <input class="form-control" type="text" name="email_usuario" id="email_usuario">
+            <h1>Autenticação</h1>
+            <hr>
+            <div class="col mt-2">
+                <label for="" style="color: dodgerblue"><b>Email:</b></label>
+                <input class="form-control" type="email" name="email_usuario" id="email_usuario" required>
+                <div class="valid-tooltip">
+                    Campo obrigatório
                 </div>
-        
-                <div class="col mt-2">
-                    <label for="" style="color: dodgerblue"><b>Senha:</b></label>
-                    <input class="form-control" type="password" name="senha_usuario" id="senha_usuario">
+            </div>
+
+            <div class="col mt-2">
+                <label for="" style="color: dodgerblue"><b>Senha:</b></label>
+                <input class="form-control" type="password" name="senha_usuario" id="senha_usuario" required>
+                <div class="valid-tooltip">
+                    Campo obrigatório
                 </div>
-        
-                <div class="col mt-4">
-                    <button class="form-control button" type="submit">
-                        <b>Autenticar</b>
-                    </button>
-                </div>
+            </div>
+
+            <div class="col mt-4">
+                <button class="form-control button" type="submit">
+                    <b>Autenticar</b>
+                </button>
+            </div>
         </div>
     </form>
 </div>
+
+<script src="/public/assets/js/bootstrap_validacao.js"></script>
