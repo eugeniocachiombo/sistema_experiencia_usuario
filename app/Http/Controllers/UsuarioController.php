b811e647d39b556da819d9e371522f74f1a34ac2
eugeniocachiombo <eugeniocachiombo@gmail.com>
2024-02-23 02:37:21 +0100

    Código melhorado Usuario controller

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -30,14 +30,13 @@ class UsuarioController extends Controller
     }
 
     public function autenticarView(Request $request)
-    {   
-        if(session("bloqueio_sistema") || session("tentativa_login") == 3){
+    {
+        if (session("bloqueio_sistema") || session("tentativa_login") == 3) {
             session()->put("bloqueio_sistema", true);
             return view("usuario.excessao_tentativas");
-        } 
-        else if(session("id_usuario")){
+        } else if (session("id_usuario")) {
             return view("usuario.pagina_inicial");
-        }else{
+        } else {
             return view("usuario.autenticacao");
         }
     }
@@ -71,68 +70,38 @@ class UsuarioController extends Controller
     public function verficarDispositivo($usuario)
     {
         $dispositvo = DispositivoController::buscarDispositivo($usuario);
-
         if ($dispositvo) {
-            $agent = new Agent();
-            $nome_dispositivo = $agent->device();
-            $nome_navegador = $agent->browser();
-            $nome_plataforma = $agent->platform();
-
-            $dispositivo_actual = $nome_dispositivo;
-            $navegador_actual = $nome_navegador . " " . $agent->version($nome_navegador);
-            $plataforma_actual = $nome_plataforma . " " . $agent->version($nome_plataforma);
-            $id_usuario_actual = $usuario->id;
-
-            $dispositvo = DispositivoController::buscarDispositivo($usuario);
-            $id_dispositivo_query = $dispositvo->id;
-            $dispositivo_query = $dispositvo->nome_dispositivo;
-            $navegador_query = $dispositvo->navegador;
-            $plataforma_query = $dispositvo->plataforma;
-            $id_usuario_query = $dispositvo->id_usuario;
-
-            $se_dispositivo_igual = $dispositivo_actual == $dispositivo_query;
-            $se_navegador_igual = $navegador_actual == $navegador_query;
-            $se_plataforma_igual = $plataforma_actual == $plataforma_query;
-            $se_usuario_igual = $id_usuario_actual == $id_usuario_query;
-
-            if ($se_dispositivo_igual && $se_navegador_igual && $se_plataforma_igual && $se_usuario_igual) {
-                $ataque = AtaqueController::buscarAtaque($usuario);
-                session()->put("total_ataques", $ataque);
-
-                session()->put("id_usuario", $usuario->id);
-                session()->put("nome_usuario", $usuario->nome_usuario);
-                session()->put("genero_usuario", $usuario->genero_usuario);
-                session()->put("email_usuario", $usuario->email_usuario);
-
-                session()->put("id_dispositivo_query", $id_dispositivo_query);
-                session()->put("dispositivo_query", $dispositivo_query);
-                session()->put("navegador_query", $navegador_query);
-                session()->put("plataforma_query", $plataforma_query);
-                setcookie("usuario_logado", "usuario_logado", 120);
-                return view("usuario.pagina_inicial");
-            } else {
-                AtaqueController::registrarAtaque($usuario);
-                return view("usuario.limite_sessoes");
-            }
-
+            return compararDadosBDPorDisposivoActual($dispositvo, $usuario);
         } else {
-            $agent = new Agent();
-            $nome_dispositivo = $agent->device();
-            $nome_navegador = $agent->browser();
-            $nome_plataforma = $agent->platform();
+            return $this->setarSessoesECookieIrPaginaInicial($usuario);
+        }
+    }
 
-            $dispositivo_actual = $nome_dispositivo;
-            $navegador_actual = $nome_navegador . " " . $agent->version($nome_navegador);
-            $plataforma_actual = $nome_plataforma . " " . $agent->version($nome_plataforma);
-            
-            DispositivoController::registrarDispositivo($usuario);
-            $dispositvo = DispositivoController::buscarDispositivo($usuario);
-            $id_dispositivo_query = $dispositvo->id;
-            session()->put("id_dispositivo_query", $id_dispositivo_query);
-            session()->put("dispositivo_query", $dispositivo_actual);
-            session()->put("navegador_query", $navegador_actual);
-            session()->put("plataforma_query", $plataforma_actual);
+    public function compararDadosBDPorDisposivoActual($dispositvo, $usuario)
+    {
+        $agent = new Agent();
+        $nome_dispositivo = $agent->device();
+        $nome_navegador = $agent->browser();
+        $nome_plataforma = $agent->platform();
 
+        $dispositivo_actual = $nome_dispositivo;
+        $navegador_actual = $nome_navegador . " " . $agent->version($nome_navegador);
+        $plataforma_actual = $nome_plataforma . " " . $agent->version($nome_plataforma);
+        $id_usuario_actual = $usuario->id;
+
+        $dispositvo = DispositivoController::buscarDispositivo($usuario);
+        $id_dispositivo_query = $dispositvo->id;
+        $dispositivo_query = $dispositvo->nome_dispositivo;
+        $navegador_query = $dispositvo->navegador;
+        $plataforma_query = $dispositvo->plataforma;
+        $id_usuario_query = $dispositvo->id_usuario;
+
+        $se_dispositivo_igual = $dispositivo_actual == $dispositivo_query;
+        $se_navegador_igual = $navegador_actual == $navegador_query;
+        $se_plataforma_igual = $plataforma_actual == $plataforma_query;
+        $se_usuario_igual = $id_usuario_actual == $id_usuario_query;
+
+        if ($se_dispositivo_igual && $se_navegador_igual && $se_plataforma_igual && $se_usuario_igual) {
             $ataque = AtaqueController::buscarAtaque($usuario);
             session()->put("total_ataques", $ataque);
 
@@ -140,10 +109,48 @@ class UsuarioController extends Controller
             session()->put("nome_usuario", $usuario->nome_usuario);
             session()->put("genero_usuario", $usuario->genero_usuario);
             session()->put("email_usuario", $usuario->email_usuario);
-            
+
+            session()->put("id_dispositivo_query", $id_dispositivo_query);
+            session()->put("dispositivo_query", $dispositivo_query);
+            session()->put("navegador_query", $navegador_query);
+            session()->put("plataforma_query", $plataforma_query);
             setcookie("usuario_logado", "usuario_logado", 120);
             return view("usuario.pagina_inicial");
+        } else {
+            AtaqueController::registrarAtaque($usuario);
+            return view("usuario.limite_sessoes");
         }
+    }
+
+    public function setarSessoesECookieIrPaginaInicial($usuario)
+    {
+        $agent = new Agent();
+        $nome_dispositivo = $agent->device();
+        $nome_navegador = $agent->browser();
+        $nome_plataforma = $agent->platform();
+
+        $dispositivo_actual = $nome_dispositivo;
+        $navegador_actual = $nome_navegador . " " . $agent->version($nome_navegador);
+        $plataforma_actual = $nome_plataforma . " " . $agent->version($nome_plataforma);
+
+        DispositivoController::registrarDispositivo($usuario);
+        $dispositvo = DispositivoController::buscarDispositivo($usuario);
+        $id_dispositivo_query = $dispositvo->id;
+        session()->put("id_dispositivo_query", $id_dispositivo_query);
+        session()->put("dispositivo_query", $dispositivo_actual);
+        session()->put("navegador_query", $navegador_actual);
+        session()->put("plataforma_query", $plataforma_actual);
+
+        $ataque = AtaqueController::buscarAtaque($usuario);
+        session()->put("total_ataques", $ataque);
+
+        session()->put("id_usuario", $usuario->id);
+        session()->put("nome_usuario", $usuario->nome_usuario);
+        session()->put("genero_usuario", $usuario->genero_usuario);
+        session()->put("email_usuario", $usuario->email_usuario);
+
+        setcookie("usuario_logado", "usuario_logado", 120);
+        return view("usuario.pagina_inicial");
     }
 
     public function criarSessaoContadoraTentativas(Request $request)
